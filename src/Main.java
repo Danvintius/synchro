74e44b0c6b621dfcb5e9fc32206d9180ead85e20
Danvintius <danila.94@mail.ru>
2023-10-15 23:18:40 +0300

    for

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        String r = generateRoute("RLRFL", 100);
+        int result = 0;
+        for (int i = 0; i < r.length(); i++) {
+            if (r.equals('R')) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
 
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
     }
 }
