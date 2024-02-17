345eb28c8d4643d9c15a59df442799bc53a18b73
Danvintius <119405034+Danvintius@users.noreply.github.com>
2024-02-17 16:13:07 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,48 @@ import java.util.Map;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] routes = new String[1000];
-        List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < routes.length; i++) {
-            routes[i] = generateRoute("RLRFL", 100);
-        }
+    
+  final static String LETTERS = "RLRFR";
 
-            for (int i = 0; i < routes.length; i++) {
-                Runnable logic = () -> {
-                    char[] arr = routes[i].toCharArray();
-                    int result = 0;
-                    for (int j = 0; j < arr.length - 1; j++) {
-                        if (arr[j] == 'R') {
-                            result++;
-                        }
+    final static  int ROUTE_LENGTH = 100;
+
+    final static int AMOUNT_OF_THREADS = 1000;
+
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
+            new Thread(() -> {
+                String route = generateRoute(LETTERS, ROUTE_LENGTH);
+                int frequency = (int) route.chars().filter(ch -> ch == 'R').count();
+
+                synchronized (sizeToFreq) {
+                    if (sizeToFreq.containsKey(frequency)) {
+                        sizeToFreq.put(frequency, sizeToFreq.get(frequency) + 1);
+                    } else {
+                        sizeToFreq.put(frequency, 1);
                     }
                 }
-            System.out.println(routes);
-            System.out.println(result);
+            }).start();
         }
 
+        Map.Entry<Integer, Integer> max = sizeToFreq
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .get();
+        System.out.println("Самое частое количество повторений " + max.getKey()
+        + " (встретилось " + max.getValue() + " раз)");
+
+        System.out.println("Другие размеры: ");
+
+        sizeToFreq
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .forEach(e -> System.out.println(" - " + e.getKey() + " (" + e.getValue() + " раз)"));
+
     }
-
-    public static final Map<Integer, Integer> sizeToFreq;
-
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
@@ -37,4 +54,5 @@ public class Main {
         }
         return route.toString();
     }
+
 }
